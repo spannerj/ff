bd5fcb9e725a98a9c11a7299e3933f3c5eeb677f
Spencer Jago <spencer.jago@landregistry.gsi.gov.uk>
2016-04-05 14:59:14 +0100

    some script changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-"# ff" 
+### Development Environment
+
+This environment will checkout the lrfantasy code and install all the required items for local development
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -6,16 +6,20 @@ yum install git -y
 #Copy script files (so everything is as would be in real environment)
 sudo -i -u vagrant bash -c "cp /vagrant/scripts/*.sh ~/ && chmod 0700 ~/*.sh"
 
-if [ ! -d "~/code" ]; then
+if [ ! -d code ]; then
 	echo "make code directory"
 	mkdir code
 fi
 
 cd code
 
-if  [ ! -L ~/code/lrfantasy ]; then
+if  [ ! -d lrfantasy ]; then
 	echo "clone repo"
 	git clone https://github.com/spannerj/lrfantasy.git
+	cd lrfantasy
+	git pull
+	git fetch
+	git checkout amend_postgres
 fi
 
 #Run project installs
